a22b5e743f9c3b6e0822a660d59b4e1babc704b9
Maulana Luthfi Nasution <93419946+ophinst@users.noreply.github.com>
2024-05-14 10:19:08 +0700

    [backend] user profile API #28 (#29)
    * wip
    
    * remove password from response body

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,9 +1,14 @@
 import * as express from "express";
 import UserController from "../controller/user.controller";
+import AuthMiddleware from "../middleware/auth.middleware";
+import UploadMiddleware from "../middleware/upload.middleware";
 
 const UserRouter = express.Router();
 
 UserRouter.get("/user", UserController.GetUser);
 UserRouter.get("/user/:id", UserController.GetUserById);
+
+UserRouter.patch("/user", AuthMiddleware.VerifyToken, UserController.UpdateUserData);
+UserRouter.patch("/user/profile", AuthMiddleware.VerifyToken, UploadMiddleware.ProcessFiles, UserController.UpdateUserProfilePicture);
 
 export default UserRouter;
